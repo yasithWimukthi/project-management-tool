d69e16ab6eab13990de1e930428b49c27ed134b8
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-01 19:57:08 +0530

    Implement navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
-                <Link to="/" className="brand-logo">MarioPlan</Link>
+                <Link to="/" className="brand-logo">ProjectManager</Link>
                 <SignedInLinks />
                 <SignedOutLinks />
             </div>
