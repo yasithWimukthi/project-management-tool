3dc2dd90ea0a171345217ebb58837fc9a9029771
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-01 21:04:10 +0530

    Implement ProjectSummary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 function ProjectList() {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project title</span>
-                    <p>posted by ...........</p>
-                    <p className="grey-text">date</p>
-                </div>
-            </div> 
+            <ProjectSummary />
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project title</span>
+                <p>posted by ...........</p>
+                <p className="grey-text">date</p>
+            </div>
+        </div> 
+    )
+}
+
+export default ProjectSummary;
