28de892efe3a218a879ca0e1bfc1a02969519cf3
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-01 20:22:01 +0530

    Implement Dashboard

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ProjectList() {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project title</span>
+                    <p>posted by ...........</p>
+                    <p className="grey-text">date</p>
+                </div>
+            </div> 
+        </div>
+    )
+}
+
+export default ProjectList;
