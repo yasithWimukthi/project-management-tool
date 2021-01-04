86cf01e95d5342f02842fd9a4116ef2921561a04
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-04 11:21:15 +0530

    Initialize firebase

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore,applyMiddleware} from 'redux';
+import {createStore,applyMiddleware,compose} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import {getFirestore} from 'redux-firestore';
-import {getFirebase} from 'react-redux-firebase';
+import {getFirestore,reduxFirestore} from 'redux-firestore';
+import {getFirebase,reactReduxFirebase} from 'react-redux-firebase';
 import rootReducer from './store/reducers/rootReducer';
+import fbConfig from './config/fbConfig';
 
-const store = createStore(rootReducer,applyMiddleware(thunk.withExtraArgument({getFirestore,getFirebase})));
+const store = createStore(rootReducer,
+  compose( 
+    applyMiddleware(thunk.withExtraArgument({getFirestore,getFirebase})),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+    )
+  );
 
 ReactDOM.render(
   <Provider store={store}>
