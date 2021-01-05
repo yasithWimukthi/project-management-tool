e8ae12cf5f9e21842c525aa3d423e59bf9a26827
yasithWimukthi <it19966922@my.sliit.lk>
2021-01-05 09:01:03 +0530

    Implement Dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import {compose} from 'redux';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
 
@@ -25,8 +27,13 @@ class Dashboard extends Component {
 
 const mapStatetoProps = (state) =>{
     return{
-        projects: state.project.projects
+        projects: state.firestore.ordered.projects
     }
 }
 
-export default connect(mapStatetoProps) (Dashboard);
+export default compose(
+    connect(mapStatetoProps),
+    firestoreConnect([
+        {collection : 'projects'}
+    ])
+)(Dashboard);
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
+import {firestoreReducer} from 'redux-firestore';
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    project : projectReducer
+    project : projectReducer,
+    firestore : firestoreReducer
 })
 
 export default rootReducer;
